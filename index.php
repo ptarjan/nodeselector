9981217b2b6298aaa5cf5edde8bd511ab509756a
Paul Tarjan <spam@paulisageek.com>
2009-04-13 11:43:15 -0700

    added stylesheet

--- index.php
+++ index.php
@@ -1,3 +1,24 @@
+<?php print '<?xml version="1.0" encoding="UTF-8"?>' ?>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+ "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+  <head>
+    <title>Node Selector Bookmarklet</title>
+    <link rel="stylesheet" type="text/css" href="http://yui.yahooapis.com/2.7.0/build/reset/reset-min.css" />
+    <link rel="stylesheet" href="/style.css" type='text/css' />  
+    
+    <link rel="icon" href="/favicon.ico" type="image/x-icon" />
+
+  </head>
+  <body>
+
+    <div id='main'>
+
+    <h1>NodeSelector Bookmarklet</h1>
+
+    <div class='content'>
+
 <ol><li>
 Right click and bookmark this: <a href='
 javascript:(
@@ -17,7 +38,9 @@ Find the interesting item on the page and click it.
 </li><li>
 See the xpath to that node.
 </li><li>
-Press ESC anytime to stop selecting nodes.
+Press <b>ESC</b> anytime to stop selecting nodes.
+</li><li>
+...
 </li><li>
 Profit.
 </li></ol>
@@ -29,3 +52,8 @@ Profit.
 </ol><ol>
 <a href='ns.js'>Read the source</a>
 </ol></ul>
+
+      </div>
+    </div>
+  </body>
+</html>
